6af4dc8e0aaed7ac0695ab368e2db65b854424ef
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-06-29 23:21:08 +0200

    created models and seed the db

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from fastapi import FastAPI
+from typing import List
+from models import User, Gender, Role
 
 
 app = FastAPI()
 
 
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Alex",
+        last_name="medai",
+        gender=Gender.male,
+        roles=[Role.admin]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Jones",
+        last_name="saint",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
+]
+
+
 @app.get("/")
 def root():
-    return {"Hello Fast APi"}
+    return {"Hello": " Fast APi"}
