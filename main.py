f2a78fe45f7d2414f4dc5c3b4a59db3116977836
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-06-30 00:22:28 +0200

    implemented delete & update user,added exception handling and status codes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List
-from models import User, Gender, Role
+from models import User, Gender, Role, UserUpdateRequest
 from uuid import UUID, uuid4
 
 
@@ -44,5 +44,44 @@ async def fetch_users():
 
 @app.post("/api/v1/users")
 async def register_user(user: User):
+    for existing_user in db:
+        if existing_user.id == user.id:
+            raise HTTPException(
+                status_code=400,
+                detail=f"User with id: {user.id} already exists"
+            )
     db.append(user)
     return {"id": user.id}
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return {"message": "User updated successfully"}
+            # db.remove(user)
+            # db.append(newUser)
+    raise HTTPException(
+        status_code=404,
+        detail=f"User with id: {user_id} does not exist"
+    )
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted successfully"}
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
